1fbbd6917ebe9bc8c0bf19bf71ed89590110ecd3
datng <nguyendatci@hotmail.com>
2018-04-08 20:05:17 +0200

    add delete and set wallpaper options for gallery

--- app/src/main/java/com/upmc/tomo/tp_alt_mozaigram/fragments/GalleryFragment.java
+++ app/src/main/java/com/upmc/tomo/tp_alt_mozaigram/fragments/GalleryFragment.java
@@ -1,20 +1,35 @@
 package com.upmc.tomo.tp_alt_mozaigram.fragments;
 
+import android.app.AlertDialog;
 import android.app.Fragment;
+import android.app.ProgressDialog;
+import android.app.WallpaperManager;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.Toast;
 
 import com.upmc.tomo.tp_alt_mozaigram.R;
 import com.upmc.tomo.tp_alt_mozaigram.adapter.SingleAlbumAdapter;
+import com.upmc.tomo.tp_alt_mozaigram.persists.Persists;
 import com.upmc.tomo.tp_alt_mozaigram.task.LoadImagesTask;
 import com.upmc.tomo.tp_alt_mozaigram.utils.Utils;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 
 /**
@@ -26,8 +41,8 @@ public class GalleryFragment extends Fragment {
 
     GridView galleryGridView;
     ArrayList<String> imageList = new ArrayList<>();
-    String album_name = "My Mozaik";
     SingleAlbumAdapter adapter;
+    ProgressDialog progress;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -53,8 +68,66 @@ public class GalleryFragment extends Fragment {
         adapter = new SingleAlbumAdapter(view.getContext(), imageList);
         galleryGridView.setAdapter(adapter);
         new LoadImagesTask(imageList, adapter, galleryGridView, getFragmentManager()).execute();
+        galleryGridView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+
+            public boolean onItemLongClick(AdapterView<?> arg0, View v,
+                                           final int index, long arg3) {
+
+                AlertDialog.Builder pictureDialog = new AlertDialog.Builder(getActivity());
+                pictureDialog.setTitle("Choose action");
+                String[] pictureDialogItems = {
+                        "Delete the picture",
+                        "Set new Wallpaper"};
+                pictureDialog.setItems(pictureDialogItems,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                switch (which) {
+                                    case 0:
+                                        String path = imageList.get(index);
+                                        File file = new File(path);
+                                        boolean deleted = false;
+                                        if (file.exists()) {
+                                            deleted = file.delete();
+                                            imageList.remove(path);
+                                        }
+                                        if (deleted) {
+                                            adapter.notifyDataSetChanged();
+                                            Toast.makeText(getActivity(), "Picture deleted !", Toast.LENGTH_SHORT).show();
+                                        }
+                                        break;
+                                    case 1:
+
+                                        progress = new ProgressDialog(getActivity());
+                                        progress.setMessage("Setting new Wallpaper");
+                                        progress.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+                                        progress.show();
+
+                                        final Thread t = new Thread() {
+                                            @Override
+                                            public void run() {
+                                                Uri uri = Uri.fromFile(new File(imageList.get(index)));
+                                                Bitmap bitmap = null;
+                                                try {
+                                                    bitmap = BitmapFactory.decodeStream(getActivity().getContentResolver().openInputStream(uri));
+                                                    WallpaperManager wallpaperManager = WallpaperManager.getInstance(getActivity());
+                                                    wallpaperManager.setBitmap(bitmap);
+                                                    progress.dismiss();
+                                                } catch (IOException e) {
+                                                    e.printStackTrace();
+                                                }
+                                            }
+                                        };
+                                        t.start();
+                                        break;
+                                }
+                            }
+                        });
+                pictureDialog.show();
+                return true;
+            }
+        });
+
         return view;
     }
-
-
 }
